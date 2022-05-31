043dac8b97ec13502f95011c464da29c4946812d
FahadAlsharif <106029791+FahadAlsharif@users.noreply.github.com>
2022-05-31 13:01:05 +0300

    Add files via upload

---
+++ Making Connections/script.js
@@ -0,0 +1,18 @@
+function yes(c) {
+    c.parentElement.parentElement.parentElement
+    .querySelector(".who")
+    .remove();
+}
+
+
+function yes2(c) {
+    c.parentElement.parentElement.parentElement
+    .querySelector(".who3")
+    .remove();
+}
+
+
+function edit(i) {
+    i.parentElement.parentElement.parentElement.querySelector(".name").innerText = 'Fahad'
+
+}
